12b20fcce78b9e0ccd9d6394e3cea95ded051c3b
Eksell <eksell@student.chalmers.se>
2015-06-01 18:57:01 +0200

    Now constructs a maze
    Seem to work but isn't very pretty...

--- Lab6/src/controller_hammer/Maze.java
+++ Lab6/src/controller_hammer/Maze.java
@@ -1,8 +1,15 @@
 package controller_hammer;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
-import model_nail.DisjointSets;
+import model_nail.*;
+import model_nail.Point.Direction;
+
 
 
 
@@ -10,38 +17,87 @@ public class Maze extends Board {
 	//Observable inherited through Board
 	private ExtendedGraph graph;
 	private DisjointSets set;
-  		
-    public Maze( int rows, int cols ) {
-    	super(rows,cols);
-    	set = new DisjointSets(rows*cols);
-    	graph = new ExtendedGraph(rows, cols);
-    	//Adds
-    	System.out.println("Maze Construct");
-    }
-    
-    public void create() { //TODO Create the maze
-    	
-    	//Added, testing relation to boarddisplay
-    	System.out.println("Create:"+super.maxCol+"*"+super.maxRow+" maze.");
 
-    	setChanged();
-   	 	notifyObservers(this);
-    	
-//    	 setChanged();
-//    	 notifyObservers(maxRow,maxCol);
-//    	 
-//    	 int knockedWalls = 0;
-//    	 //while(knockedWalls < (maxCell-1)();//TODO
-    }
-    
-    public void search() { //TODO Later,  Maze search
-    	 graph.unweighted(0);
-//	     List<Integer> list = graph.getPath(maxCell-1);
-//    	 for(Integer i : list){
-//    		 setChanged();
-//    		 notifyObservers(i);
-//    	 }
-    }
-    
-//    ...
+	public Maze( int rows, int cols ) {
+		super(rows,cols);
+		set = new DisjointSets(rows*cols);
+		graph = new ExtendedGraph(rows, cols);
+		//Adds
+		System.out.println("Maze Construct");
+	}
+
+	public void create() { //TODO Create the maze
+
+		//Added, testing relation to boarddisplay
+		System.out.println("Create:"+super.maxCol+"*"+super.maxRow+" maze.");
+
+		//Notify observers of this maze-instance
+		setChanged();
+		notifyObservers(this);
+
+		/**
+		 * Har DJ-set som här loopas till att ha unioner ges riktningar
+		 */
+
+		Random rand = new Random(); // To get a rng for the direction
+		List<Direction> dirList = new ArrayList<Direction>();
+		dirList.add(Direction.UP);
+		dirList.add(Direction.DOWN);
+		dirList.add(Direction.RIGHT);
+		dirList.add(Direction.LEFT);
+
+		HashMap<Integer, Direction> mazeMap = new HashMap<Integer, Direction>();
+
+		int wallsLeft = 0;	// walls left to knock down
+		int cell = 0;
+		Point currentPoint = new Point(0,0);
+
+
+		while(cell < (maxCell-1)){
+			System.out.print("W:"+cell+" of "+ maxCell);
+			currentPoint = new Point(getRow(cell),getCol(cell));
+
+			while(true){
+
+				Direction d = dirList.get(rand.nextInt(3)); //Random direction to move in
+				int oldPoint = cell;
+				int newPoint;
+				currentPoint = new Point(getRow(cell),getCol(cell));
+
+				Point testPoint = new Point(getRow(cell),getCol(cell));
+				testPoint.move(d);
+
+				if(0 <= getCellId(testPoint) && getCellId(testPoint)<= (maxCell-1)){
+					currentPoint.move(d);
+					newPoint = getCellId(currentPoint);
+					System.out.print("["+oldPoint+"->"+newPoint+"] ");
+				}else continue;
+
+				if(set.find(newPoint) != set.find(oldPoint)){
+					System.out.print("C:"+cell+" "+d+"\n");
+					mazeMap.put(getCellId(currentPoint), d); // Connect position and direction
+					set.union(set.find(oldPoint),set.find(newPoint));
+					cell++;
+					break;
+				}
+
+
+			}
+
+		}
+
+		//    	 int knockedWalls = 0;
+		//    	 //while(knockedWalls < (maxCell-1)();//TODO
+	}
+
+	public void search() { //TODO Later,  Maze search
+		graph.unweighted(0);
+		//	     List<Integer> list = graph.getPath(maxCell-1);
+		//    	 for(Integer i : list){
+		//    		 setChanged();
+		//    		 notifyObservers(i);
+		//    	 }
+	}
+
+	//    ...
 }
