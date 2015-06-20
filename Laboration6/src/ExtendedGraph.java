940ba845d5cc442f9e00fe6b733dcda44f5b0c18
aabri <anna.abri@gmail.com>
2015-06-21 01:53:18 +0200

    project Maze

---
+++ Laboration6/src/ExtendedGraph.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ExtendedGraph extends Graph {
+	
+	public List<Integer> getPath( int destName ) {
+		
+		unweighted(0);
+		
+		Vertex ver = vertexMap.get( destName );
+        List<Integer> l = new ArrayList<Integer>();
+		return getPath( ver, l );
+	}
+	
+	private List<Integer> getPath( Vertex dest, List<Integer> l ) {
+		if( dest == null )
+			return l;
+		
+		l.add(dest.name);
+		return getPath( dest.prev, l);
+	}
+}
---
+++ Laboration6/src/Maze.java
@@ -0,0 +1,126 @@
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+
+
+
+public class Maze extends Board {
+	//Observable inherited through Board
+	private ExtendedGraph graph;
+	private DisjointSets set;
+	private int col, row, amountOfSets;
+	private boolean connect;
+	private Point.Direction d;
+
+	public Maze( int rows, int cols ) {
+		super(rows,cols);
+		row = rows;
+		col = cols;
+
+		//Adds
+		System.out.println("Maze Construct");
+	}
+
+	public void create() { 
+		
+		graph = new ExtendedGraph();
+		set = new DisjointSets(maxCell);
+		amountOfSets = maxCell;
+		
+		
+		while( amountOfSets > 1){
+			
+			
+			//picking a random direction
+			Random rand = new Random(); 
+			
+			int i;
+		
+				i = rand.nextInt(4);
+				if	(i == 0){	
+					d = Point.Direction.UP ;
+				}
+				
+				if	(i == 1){ 	
+					d = Point.Direction.LEFT;
+				}
+				
+				if(i == 2) {	
+					d = Point.Direction.RIGHT;
+				}
+				
+				else   { 	
+					d = Point.Direction.DOWN;
+				}
+				
+				//Creating two points on the same random cell inside grid
+				Random randCell = new Random();
+				
+				int a = randCell.nextInt(maxCell);
+				
+				Point currentPoint = new Point(getRow(a),getCol(a)); 
+				Point newPoint = new Point(getRow(a),getCol(a));
+				
+				
+				
+				//moves one of the points in the random direction determined above from the other point 
+				 
+				newPoint.move(d);
+				
+				//Check if newPoint is still inside the original grid, if so, check if cells are already connected
+				//or if we should do a new union
+				if(isValid(newPoint))
+				{
+				
+			
+			
+			int currentId = getCellId(currentPoint);
+			int newId = getCellId(newPoint);
+			int set1 = set.find(currentId);
+			int set2 = set.find(newId);
+			
+			if( set1 != set2){
+				set.union(set1, set2);
+				amountOfSets--;
+				//store the edges in both directions in graph
+				graph.addEdge(currentId, newId, 1);
+				graph.addEdge(newId, currentId, 1);
+				
+				Pair<Integer, Point.Direction> pair = new Pair<Integer, Point.Direction>(currentId, d);
+    			setChanged();
+    			notifyObservers(pair);
+			}
+			
+			}	
+			
+			
+		}
+
+			
+			
+			
+			
+			
+		}
+		
+	
+	
+	public void search() {
+		
+		//get shortest path
+    	List<Integer> path = graph.getPath(maxCell - 1);
+    	
+    	for( Integer cellId : path ) {
+        	setChanged();
+        	notifyObservers(cellId);
+    	}
+    }
+
+
+}
+
